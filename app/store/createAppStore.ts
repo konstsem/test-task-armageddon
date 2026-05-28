24b9730ef7938608849821314655a1c752afaba2
konstsem <konstantin.semenov.2@gmail.com>
2026-05-28 15:22:06 +0300

    feat: implement adding items to the cart

--- app/store/createAppStore.ts
+++ app/store/createAppStore.ts
@@ -9,18 +9,29 @@ export type AppState = {
 };
 
 export type AppStore = AppState & {
+    cart: { byId: Record<string, { id: string; name: string }>; allIds: string[] };
     addItems: (items: AsteroidItem[]) => void;
+    addToCart: ({ id, name }: { id: string; name: string }) => void;
+    clearCart: () => void;
 };
 
 export const createAppStore = (initialState: AppState) => {
     return makeAutoObservable<AppStore>({
         items: initialState.items,
+        cart: { byId: {}, allIds: [] },
 
         addItems(newItems: AsteroidItem[]) {
             newItems.forEach((item) => {
                 this.items.byId[item.id] = item;
             });
             this.items.allIds.push(...newItems.map((item) => item.id));
+        },
+        addToCart({ id, name }: { id: string; name: string }) {
+            this.cart.allIds.push(id);
+            this.cart.byId[id] = { id, name };
+        },
+        clearCart() {
+            this.cart = { byId: {}, allIds: [] };
         },
     });
 };
