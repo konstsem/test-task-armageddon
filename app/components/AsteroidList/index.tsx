8d33643833e58c40e8098a5a075a218904336b3a
konstsem <konstantin.semenov.2@gmail.com>
2026-05-28 16:00:32 +0300

    feat: implement distance format toggling

--- app/components/AsteroidList/index.tsx
+++ app/components/AsteroidList/index.tsx
@@ -1,12 +1,21 @@
 "use client";
+import { observer } from "mobx-react-lite";
 import styles from "./styles.module.css";
 import { AsteroidCard } from "../Asteroid";
 import { useAppStore } from "@/app/store/AppStoreProvider";
 
-export const AsteroidList = () => {
+export const AsteroidList = observer(() => {
+    const store = useAppStore();
     const {
         items: { allIds },
-    } = useAppStore();
+        distanceFormat,
+    } = store;
+
+    const handleToggleDistanceFormat = (newDistanceFormat: "kilometers" | "lunar") => {
+        if (newDistanceFormat !== distanceFormat) {
+            store.toggleDistanceFormat();
+        }
+    };
 
     return (
         <main className={styles.asteroidsMain} id="main-content">
@@ -14,11 +23,21 @@ export const AsteroidList = () => {
                 <h2 className={styles.asteroidsMainTitle}>Ближайшие подлёты астероидов</h2>
 
                 <div className={styles.unitSwitcher} role="group" aria-label="Единицы измерения расстояния">
-                    <button type="button" className={styles.unitSwitcherBtn} aria-pressed="true">
+                    <button
+                        type="button"
+                        className={styles.unitSwitcherBtn}
+                        aria-pressed={distanceFormat === "kilometers"}
+                        onClick={() => handleToggleDistanceFormat("kilometers")}
+                    >
                         в километрах
                     </button>
                     <span aria-hidden="true">|</span>
-                    <button type="button" className={styles.unitSwitcherBtn} aria-pressed="false">
+                    <button
+                        type="button"
+                        className={styles.unitSwitcherBtn}
+                        aria-pressed={distanceFormat === "lunar"}
+                        onClick={() => handleToggleDistanceFormat("lunar")}
+                    >
                         в лунных орбитах
                     </button>
                 </div>
@@ -31,4 +50,4 @@ export const AsteroidList = () => {
             </ul>
         </main>
     );
-};
+});
